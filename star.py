ee2a484b0d25c98a3dab9ddc1f660c25b9d8052b
navyap11 <ashwin.p.parmar@gmail.com>
2025-02-26 15:47:48 +0100

    f

---
+++ star.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("black")
+star=turtle.Turtle()
+star.color("cyan")
+star.speed(7.253)
+
+for i in range(5):
+    star.forward(150.12345)
+    star.left(144)
+
+turtle.done()
